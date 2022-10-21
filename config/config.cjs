6f96e758222a4a847b21f92f5af03b02a93dbfae
Nguyen Huu Tien <nghuutien0912@gmail.com>
2022-10-21 22:58:28 +0700

    fix: add README.md file and fix some db config

--- config/config.cjs
+++ config/config.cjs
@@ -1,8 +1,9 @@
-{
+require('dotenv').config()
+module.exports = {
   "development": {
-    "username": "root",
-    "password": "tien",
-    "database": "group32db",
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
     "host": "127.0.0.1",
     "dialect": "mysql",
     "logging": false
