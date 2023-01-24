e215176ee57971797db13692c7abf54d4a7597a8
TienHunter <tienkbtnhp@gmail.com>
2023-01-24 21:36:07 +0700

    fix import roomService

--- src/modules/user/user.controller.js
+++ src/modules/user/user.controller.js
@@ -2,8 +2,8 @@ import { HttpStatus, UserRole, UserStatus } from "../../constant.js";
 import userService from "./services/user.service.js";
 import ErrorResponse from "../../utils/ErrorResponse.js";
 import SuccessResponse from "../../utils/SuccessResponse.js";
-import roomService from '../room/services/room.service.js';
-import { hashString, checkHashedString } from '../../middlewares/bcrypt.js';
+import * as roomService from "../room/services/room.service.js";
+import { hashString, checkHashedString } from "../../middlewares/bcrypt.js";
 const getProfile = async (req, res, next) => {
    try {
       const userProfile = await userService.getUserById(req.user.id);
@@ -27,7 +27,7 @@ const updateProfile = async (req, res) => {
       await userService.updateUserById(userId, body);
       const updatedUser = await userService.getUserById(userId);
       delete updatedUser.dataValues.password;
-      return res.status(HttpStatus.OK).json(new SuccessResponse(updatedUser))
+      return res.status(HttpStatus.OK).json(new SuccessResponse(updatedUser));
    } catch (error) {
       return res
          .status(HttpStatus.INTERNAL_SERVER_ERROR)
@@ -35,22 +35,29 @@ const updateProfile = async (req, res) => {
             new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, error.message)
          );
    }
-}
+};
 
 const changePassword = async (req, res) => {
    try {
       const userId = req.user.id;
       const { newPassword, oldPassword } = req.body;
       const currentUser = await userService.getUserById(userId);
-      if (!currentUser || !checkHashedString(oldPassword, currentUser.password)) {
-         res.status(HttpStatus.BAD_REQUEST).json(new ErrorResponse(HttpStatus.BAD_REQUEST, 'Invalid password'))
+      if (
+         !currentUser ||
+         !checkHashedString(oldPassword, currentUser.password)
+      ) {
+         res.status(HttpStatus.BAD_REQUEST).json(
+            new ErrorResponse(HttpStatus.BAD_REQUEST, "Invalid password")
+         );
          return;
       }
 
-      await userService.updateUserById(userId, { password: hashString(newPassword) })
+      await userService.updateUserById(userId, {
+         password: hashString(newPassword),
+      });
       const updatedUser = await userService.getUserById(userId);
       delete updatedUser.dataValues.password;
-      return res.status(HttpStatus.OK).json(new SuccessResponse(updatedUser))
+      return res.status(HttpStatus.OK).json(new SuccessResponse(updatedUser));
    } catch (error) {
       return res
          .status(HttpStatus.INTERNAL_SERVER_ERROR)
@@ -58,23 +65,29 @@ const changePassword = async (req, res) => {
             new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, error.message)
          );
    }
-}
+};
 
 const createUser = async (req, res) => {
    const isEmailExist = await userService.getUserByEmail(req.body.email);
    if (isEmailExist) {
-      res.status(HttpStatus.BAD_REQUEST).json(new ErrorResponse(HttpStatus.BAD_REQUEST, 'Email is already existed'));
+      res.status(HttpStatus.BAD_REQUEST).json(
+         new ErrorResponse(HttpStatus.BAD_REQUEST, "Email is already existed")
+      );
       return;
    }
 
-   const newUser = await userService.createUser({ ...req.body, role: UserRole.STUDENT, status: UserStatus.ACTIVE });
-   return res.status(HttpStatus.OK).json(new SuccessResponse(newUser))
-}
+   const newUser = await userService.createUser({
+      ...req.body,
+      role: UserRole.STUDENT,
+      status: UserStatus.ACTIVE,
+   });
+   return res.status(HttpStatus.OK).json(new SuccessResponse(newUser));
+};
 
 const getUserList = async (req, res) => {
    try {
       const userList = await userService.getUserList(req.query);
-      return res.status(HttpStatus.OK).json(new SuccessResponse(userList))
+      return res.status(HttpStatus.OK).json(new SuccessResponse(userList));
    } catch (error) {
       return res
          .status(HttpStatus.INTERNAL_SERVER_ERROR)
@@ -82,7 +95,7 @@ const getUserList = async (req, res) => {
             new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, error.message)
          );
    }
-}
+};
 
 const updateUser = async (req, res) => {
    try {
@@ -90,18 +103,24 @@ const updateUser = async (req, res) => {
       const user = await userService.getUserById(userId);
       if (!user) {
          res.status(HttpStatus.NOT_FOUND).json(
-            new ErrorResponse(HttpStatus.ITEM_NOT_FOUND, { key: 'userId', message: 'Not found user' })
-         )
+            new ErrorResponse(HttpStatus.ITEM_NOT_FOUND, {
+               key: "userId",
+               message: "Not found user",
+            })
+         );
       }
       if (user.role === UserRole.ADMIN && req.user.role === UserRole.ADMIN) {
          res.status(HttpStatus.FORBIDDEN).json(
-            new ErrorResponse(HttpStatus.UNPROCESSABLE_ENTITY, { key: 'userId', message: 'Cannot update another admin' })
-         )
+            new ErrorResponse(HttpStatus.UNPROCESSABLE_ENTITY, {
+               key: "userId",
+               message: "Cannot update another admin",
+            })
+         );
       }
       await userService.updateUserById(userId, req.body);
       const updatedUser = await userService.getUserById(userId);
       delete updatedUser.dataValues.password;
-      return res.status(HttpStatus.OK).json(new SuccessResponse(updatedUser))
+      return res.status(HttpStatus.OK).json(new SuccessResponse(updatedUser));
    } catch (error) {
       return res
          .status(HttpStatus.INTERNAL_SERVER_ERROR)
@@ -109,7 +128,7 @@ const updateUser = async (req, res) => {
             new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, error.message)
          );
    }
-}
+};
 // user for inactive an user => process related room
 // TODO: Update after
 const updateUserStatus = async (req, res) => {
@@ -118,18 +137,24 @@ const updateUserStatus = async (req, res) => {
       const user = await userService.getUserById(userId);
       if (!user) {
          res.status(HttpStatus.NOT_FOUND).json(
-            new ErrorResponse(HttpStatus.ITEM_NOT_FOUND, { key: 'userId', message: 'Not found user' })
-         )
+            new ErrorResponse(HttpStatus.ITEM_NOT_FOUND, {
+               key: "userId",
+               message: "Not found user",
+            })
+         );
       }
       if (user.role === UserRole.ADMIN && req.user.role === UserRole.ADMIN) {
          res.status(HttpStatus.FORBIDDEN).json(
-            new ErrorResponse(HttpStatus.UNPROCESSABLE_ENTITY, { key: 'userId', message: 'Cannot update another admin' })
-         )
+            new ErrorResponse(HttpStatus.UNPROCESSABLE_ENTITY, {
+               key: "userId",
+               message: "Cannot update another admin",
+            })
+         );
       }
       await userService.updateUserById(userId, req.body);
       const updatedUser = await userService.getUserById(userId);
       delete updatedUser.dataValues.password;
-      return res.status(HttpStatus.OK).json(new SuccessResponse(updatedUser))
+      return res.status(HttpStatus.OK).json(new SuccessResponse(updatedUser));
    } catch (error) {
       return res
          .status(HttpStatus.INTERNAL_SERVER_ERROR)
@@ -137,7 +162,7 @@ const updateUserStatus = async (req, res) => {
             new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, error.message)
          );
    }
-}
+};
 
 const deleteUser = async (req, res) => {
    try {
@@ -145,11 +170,16 @@ const deleteUser = async (req, res) => {
       const user = await userService.getUserById(userId);
       if (!user) {
          res.status(HttpStatus.NOT_FOUND).json(
-            new ErrorResponse(HttpStatus.ITEM_NOT_FOUND, { key: 'userId', message: 'Not found user' })
-         )
+            new ErrorResponse(HttpStatus.ITEM_NOT_FOUND, {
+               key: "userId",
+               message: "Not found user",
+            })
+         );
       }
       await userService.softDeleteUserById(userId);
-      return res.status(HttpStatus.OK).json(new SuccessResponse({ id: userId }))
+      return res
+         .status(HttpStatus.OK)
+         .json(new SuccessResponse({ id: userId }));
    } catch (error) {
       return res
          .status(HttpStatus.INTERNAL_SERVER_ERROR)
@@ -157,7 +187,7 @@ const deleteUser = async (req, res) => {
             new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, error.message)
          );
    }
-}
+};
 
 const getUserByRoomId = async (req, res) => {
    try {
@@ -166,17 +196,25 @@ const getUserByRoomId = async (req, res) => {
          return res
             .status(HttpStatus.BAD_REQUEST)
             .json(
-               new ErrorResponse(HttpStatus.UNPROCESSABLE_ENTITY, { key: 'roomId', message: "Room not exist" })
+               new ErrorResponse(HttpStatus.UNPROCESSABLE_ENTITY, {
+                  key: "roomId",
+                  message: "Room not exist",
+               })
             );
       }
       let userList = await userService.getUserByRoomId(req.params.roomId);
-      userList = userList.map(user => {
-         delete user.dataValues.password
-         return user
-      })
-      return res.status(HttpStatus.OK).json(
-         new SuccessResponse({ items: userList, totalItems: userList.length })
-      )
+      userList = userList.map((user) => {
+         delete user.dataValues.password;
+         return user;
+      });
+      return res
+         .status(HttpStatus.OK)
+         .json(
+            new SuccessResponse({
+               items: userList,
+               totalItems: userList.length,
+            })
+         );
    } catch (error) {
       return res
          .status(HttpStatus.INTERNAL_SERVER_ERROR)
@@ -184,7 +222,7 @@ const getUserByRoomId = async (req, res) => {
             new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, error.message)
          );
    }
-}
+};
 
 export default {
    getProfile,
@@ -195,5 +233,5 @@ export default {
    updateUserStatus,
    deleteUser,
    getUserByRoomId,
-   createUser
+   createUser,
 };
